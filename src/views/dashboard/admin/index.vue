570e70650e75b13df67105319836d4afef7d03b7
liuxiaoyong <liuxiaoyong@meicai.cn>
2017-10-23 08:37:48 +0800

    add eslint check

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,19 +1,20 @@
 <template>
 	<div class="dashboard-editor-container">
+    this is the admin dashboard.
 	</div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
-	export default {
-  	name: 'dashboard-admin',
-  	computed: {
-    	...mapGetters([
-      	'name',
-      	'avatar',
-      	'roles'
-    	])
-  	}
-	}
+export default {
+  name: 'dashboard-admin',
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+      'roles'
+    ])
+  }
+}
 </script>
